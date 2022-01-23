d62d1f65735d72019dd506a61de7fcb0e6f901cd
Mike Carter <avidflyer@me.com>
2022-01-22 21:45:52 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme dummy!',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => root,
-  }
 }
